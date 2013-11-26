8841f8dd675b60709b3a5da4a71438585cb60906
Andrew Guard <andrew.s.guard@gmail.com>
2013-11-26 11:19:02 -0600

    clear cache on page update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -49,6 +49,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        expire_action action:[:index,:show]
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render json: @post, status: :created, location: @post }
       else
