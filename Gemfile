ab82a6ea8b0068d4f13d973f54c63b9f3f49ecae
Andrew Guard <andrew.s.guard@gmail.com>
2013-11-26 10:19:55 -0600

    set up memcache

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ end
 
 gem 'jquery-rails'
 
+gem 'dalli'
+gem 'memcachier'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,6 +56,8 @@ RailsBlog::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found)
+
+  config.cache_store = :dalli_store
   config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners
